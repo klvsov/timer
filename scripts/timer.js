0411c4dfd8b928362d3b23ed7f63843164942822
klvsov <klvsov@gmail.com>
2020-06-18 12:43:08 +0300

    created all files

---
+++ scripts/timer.js
@@ -0,0 +1,65 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    let deadline = '2020-06-20';
+    
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+            if (t <= 0) {
+                seconds = 0;
+                minutes = 0;
+                hours = 0;
+            }
+
+            // with days
+            // hours = Math.floor((t / 1000 / 60 / 60) % 24);
+            // days = Math.floor((t / (1000 * 60 * 60 * 24))
+
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endtime);
+            console.log(t);
+            if (t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            } else {
+                hours.textContent = t.hours;
+            }
+
+            if (t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            } else {
+                minutes.textContent = t.minutes;
+            }
+
+            if (t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            } else {
+                seconds.textContent = t.seconds;
+            }
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline)
+
+});
